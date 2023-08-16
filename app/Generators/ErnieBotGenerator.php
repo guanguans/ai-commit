e6da2de22033f92ad829a113337ad5aba78c8530
yaozm <ityaozm@gmail.com>
2023-08-16 18:31:16 +0800

    feat(Generators): Add ErnieBotGenerator
    - Add ErnieBotGenerator class
    - Implement GeneratorContract interface
    - Initialize config, ernie, and outputStyle properties
    - Implement generate method
    - Implement getCompletionMessages method
    - Implement buildWriter method

---
+++ app/Generators/ErnieBotGenerator.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/ai-commit.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Generators;
+
+use App\Contracts\GeneratorContract;
+use App\Support\Ernie;
+use Illuminate\Console\OutputStyle;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+class ErnieBotGenerator implements GeneratorContract
+{
+    /**
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * @var \App\Support\Ernie
+     */
+    protected $ernie;
+
+    /**
+     * @var \Illuminate\Console\OutputStyle
+     */
+    protected $outputStyle;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+        $this->ernie = new Ernie(Arr::only($config, ['http_options', 'retry', 'base_url', 'api_key', 'secret_key']));
+        $this->outputStyle = resolve(OutputStyle::class);
+    }
+
+    /**
+     * @psalm-suppress RedundantCast
+     */
+    public function generate(string $prompt): string
+    {
+        $prompt = 'PHP是什么？';
+
+        $parameters = [
+            'messages' => [
+                ['role' => 'user', 'content' => $prompt],
+            ],
+            'user' => Str::uuid()->toString(),
+        ] + Arr::get($this->config, 'parameters', []);
+
+        $response = $this->ernie->ernieBot($parameters, $this->buildWriter($messages));
+
+        // fake 响应
+        return (string) ($messages ?? $this->getCompletionMessages($response));
+    }
+
+    /**
+     * @param array|\ArrayAccess $response
+     */
+    protected function getCompletionMessages($response): string
+    {
+        return Arr::get($response, 'result', '');
+    }
+
+    /**
+     * @noinspection JsonEncodingApiUsageInspection
+     */
+    protected function buildWriter(?string &$messages): \Closure
+    {
+        return function (string $data) use (&$messages): void {
+            // 流响应完成
+            if (str($data)->startsWith('data: [DONE]')) {
+                return;
+            }
+
+            // (正常|错误|流)响应
+            $rowResponse = (array) json_decode(Ernie::sanitizeData($data), true);
+            $messages .= $text = $this->getCompletionMessages($rowResponse);
+            $this->outputStyle->write($text);
+        };
+    }
+}
