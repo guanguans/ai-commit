ecf01865491bded2e18fc1ba32bc4fd1cc28e219
ityaozm@gmail.com <yaozm>
2023-11-09 14:46:36 +0800

    chore(monorepo-builder): Remove unnecessary require statement
    - Remove commented out require statement

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -26,7 +26,6 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorke
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
-    // require __DIR__.'/vendor/autoload.php';
     require __DIR__.'/vendor-bin/monorepo-builder-worker/vendor/guanguans/monorepo-builder-worker/bin/namespace-prefix-fixer.php';
     require __DIR__.'/vendor-bin/monorepo-builder-worker/vendor/autoload.php';
     $mbConfig->defaultBranch('main');
